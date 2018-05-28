cde24e2ac419b3b21d413c44ab757c7c98f5c1e9
Jeremy Lewi <jeremy@lewi.us>
2018-05-28 16:27:30 -0700

    Create a new group to act as owners of kf-demo-owner.

--- sync_groups.sh
+++ sync_groups.sh
@@ -3,4 +3,5 @@
 # Sync groups using the gam CLI
 set -ex
 
+gam update group kf-demo-owners@kubeflow.org sync member file kf-demo-owners.members.txt
 gam update group devrel-team@kubeflow.org sync member file devrel-team.members.txt
