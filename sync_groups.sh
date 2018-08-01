8cd49c6acfb1e1ecfbd64bcc56d0f8008d69ef93
Jeremy Lewi <jeremy@lewi.us>
2018-08-01 15:59:14 -0700

    Update google groups.
    * Sync members of the google-kubeflow-team.
    * Create a new team for Googlers with admin privileges on kubeflow.org resources

--- sync_groups.sh
+++ sync_groups.sh
@@ -3,5 +3,7 @@
 # Sync groups using the gam CLI
 set -ex
 
+gam update group google-kubeflow-admins@kubeflow.org sync member file google-kubeflow-admins.members.txt
+gam update group google-team@kubeflow.org sync member file google-team.members.txt
 gam update group kf-demo-owners@kubeflow.org sync member file kf-demo-owners.members.txt
 gam update group devrel-team@kubeflow.org sync member file devrel-team.members.txt
