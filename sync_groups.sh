a88fe955eeb20ae18f6a591f15f0ce6290006e77
Jeremy Lewi <jeremy@lewi.us>
2018-08-30 11:13:59 -0700

    Create a file to manage members of the release team.

--- sync_groups.sh
+++ sync_groups.sh
@@ -3,6 +3,7 @@
 # Sync groups using the gam CLI
 set -ex
 
+gam update group release-team@kubeflow.org sync member file release-team.members.txt
 gam update group google-kubeflow-admins@kubeflow.org sync member file google-kubeflow-admins.members.txt
 gam update group google-team@kubeflow.org sync member file google-team.members.txt
 gam update group kf-demo-owners@kubeflow.org sync member file kf-demo-owners.members.txt
