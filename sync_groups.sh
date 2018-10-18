c50650d4a0c4448f5338c5ef190e6bb81006f734
Jeremy Lewi <jeremy@lewi.us>
2018-10-17 20:47:04 -0700

    Define a group for ci-team@kubeflow.org

--- sync_groups.sh
+++ sync_groups.sh
@@ -3,6 +3,7 @@
 # Sync groups using the gam CLI
 set -ex
 
+gam update group ci-team@kubeflow.org sync member file ci-team.members.txt
 gam update group release-team@kubeflow.org sync member file release-team.members.txt
 gam update group google-kubeflow-admins@kubeflow.org sync member file google-kubeflow-admins.members.txt
 gam update group google-team@kubeflow.org sync member file google-team.members.txt
