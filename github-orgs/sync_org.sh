eb919ebc98ab288c230d258452cf8ab342e14325
Jeremy Lewi <jeremy@lewi.us>
2018-12-07 06:06:51 -0800

    Add Thea as GitHub Admin.
    * Thea works for Google and is a community manager for Kubeflow.
    * She will be helping administer the GitHub org so she needs to be an admin.
    
    * Update sync_org.sh to handle admins
    * Change sync_org.sh command line argument format to --<name>=<value>

--- github-orgs/sync_org.sh
+++ github-orgs/sync_org.sh
@@ -7,36 +7,60 @@
 set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-TEST_INFRA_DIR=$1
-TOKEN_FILE=$2
-CONFIRM=$3
-usage() {
-	echo "sync_org.sh <kubernets-test-infra-dir> <path-to-github-token>"
+parseArgs() {
+  # Parse all command line options
+  while [[ $# -gt 0 ]]; do
+    # Parameters should be of the form
+    # --{name}=${value}
+    echo parsing "$1"
+    if [[ $1 =~ ^--(.*)=(.*)$ ]]; then
+    	name=${BASH_REMATCH[1]}
+    	value=${BASH_REMATCH[2]}
+     	eval ${name}="${value}"
+    elif [[ $1 =~ ^--(.*)$ ]]; then
+		name=${BASH_REMATCH[1]}
+		value=true
+		eval ${name}="${value}"
+    else
+    	echo "Argument $1 did not match the pattern --{name}={value} or --{name}"
+    fi
+    shift
+  done
 }
 
-if [ -z ${TEST_INFRA_DIR} ]; then
+parseArgs $*
+
+usage() {
+	echo "sync_org.sh --test_infra_dir=<kubernetes-test-infra-dir> --token_file=<path-to-github-token> [--confirm] [--admins]"
+}
+
+if [ -z ${test_infra_dir} ]; then
 	usage
 	exit 1	
 fi
 
-if [ -z ${TOKEN_FILE} ]; then
+if [ -z ${token_file} ]; then
 	usage
 	exit 1	
 fi
 
-if [ -z ${CONFIRM} ]; then
-	echo CONFIRM not set defaulting to dryrun mode
-    CONFIRM=false
+if [ -z ${confirm} ]; then
+	echo confirm not set defaulting to dryrun mode
+    confirm=false
 fi	
 pushd .
-cd ${TEST_INFRA_DIR}
+cd ${test_infra_dir}
 
-bazel run //prow/cmd/peribolos -- --fix-org-members --config-path ${DIR}/kubeflow/org.yaml \
-	--github-token-path ${TOKEN_FILE} \
+if ${admins}; then
+	FIX_ADMINS=--fix-org-members
+fi
+
+bazel run //prow/cmd/peribolos -- --fix-org-members ${FIX_ADMINS} --config-path ${DIR}/kubeflow/org.yaml \
+	--github-token-path ${token_file} \
 	--required-admins=jlewi \
 	--required-admins=abhi-g \
 	--required-admins=google-admin \
 	--required-admins=googlebot \
 	--required-admins=richardsliu \
 	--required-admins=vicaire \
-	--confirm=${CONFIRM}
+	--confirm=${confirm}
