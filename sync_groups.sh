8fee4f5fc1b8c5c0b232c04a8942d759df8d44b2
Jeremy Lewi <jeremy@lewi.us>
2019-01-04 16:46:04 -0800

    Create a google group for folks at the Insight Data Scient Fellows program.
    * Some folks will be kicking the tires on Kubeflow.
    
    * Add jeremy@lewi.us to test IAP via googlegroups. Will remove in follow on PR.

--- sync_groups.sh
+++ sync_groups.sh
@@ -3,7 +3,7 @@
 # Sync groups using the gam CLI
 set -ex
 
-KF_GROUPS=( "ci-team" "release-team" "google-kubeflow-admins" "google-team" "kf-demo-owners" "devrel-team" "modeldb-team" "code-search-team" "kubeflow-examples-gcr-writers" )
+KF_GROUPS=( "ci-team" "release-team" "google-kubeflow-admins" "google-team" "insight-fellowship" "kf-demo-owners" "devrel-team" "modeldb-team" "code-search-team" "kubeflow-examples-gcr-writers" )
 for g in "${KF_GROUPS[@]}"
 do
 	gam update group ${g}@kubeflow.org sync member file ${g}.members.txt
