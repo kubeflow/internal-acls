d69a5fd05269d536be93ebf55b11ceae771f183d
Jeremy Lewi <jeremy+github@lewi.us>
2019-01-15 18:13:49 -0800

    Setup permissions on project kf-feast. (#37)
    * This project will be used by the folks at GoJek and Google PSO to
      develop and test feast.
    
    Related to kubeflow/testing#254

--- sync_groups.sh
+++ sync_groups.sh
@@ -3,7 +3,7 @@
 # Sync groups using the gam CLI
 set -ex
 
-KF_GROUPS=( "ci-team" "release-team" "google-kubeflow-admins" "google-team" "insight-fellowship" "kf-demo-owners" "devrel-team" "modeldb-team" "code-search-team" "kubeflow-examples-gcr-writers" )
+KF_GROUPS=( "ci-team" "code-search-team" "devrel-team" "feast-team" "google-kubeflow-admins" "google-team" "insight-fellowship" "kf-demo-owners" "kubeflow-examples-gcr-writers" "modeldb-team" "release-team" )
 for g in "${KF_GROUPS[@]}"
 do
 	gam update group ${g}@kubeflow.org sync member file ${g}.members.txt
