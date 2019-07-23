dc2208d851fb3d15f26b16d655f6d00757c0759f
Jeremy Lewi <jeremy@lewi.us>
2019-07-23 06:29:58 -0700

    Grant example maintainers access to clusters in kubeflow-ci-deployment.
    * Examples will run on auto-deployed clusters in project kubeflow-ci-deployment
    * To facilitate debugging tests we want example maintainers to be able to
      access those clusters
    * create a new group example-maintainers to keep track of who should have
      access
    * Add @jinchihe since he's been making a lot of contributions
    
    Related to kubeflow/testing#588

--- sync_groups.sh
+++ sync_groups.sh
@@ -3,7 +3,7 @@
 # Sync groups using the gam CLI
 set -ex
 
-KF_GROUPS=( "ci-team" "code-search-team" "devrel-team" "devstats" "feast-team" "github-team" "google-kubeflow-admins" "google-team" "insight-fellowship" "kf-demo-owners" "kubeflow-examples-gcr-writers" "modeldb-team" "release-team" )
+KF_GROUPS=( "ci-team" "code-search-team" "devrel-team" "devstats" "example-maintainers" "feast-team" "github-team" "google-kubeflow-admins" "google-team" "insight-fellowship" "kf-demo-owners" "kubeflow-examples-gcr-writers" "modeldb-team" "release-team" )
 for g in "${KF_GROUPS[@]}"
 do
 	gam update group ${g}@kubeflow.org sync member file ${g}.members.txt
