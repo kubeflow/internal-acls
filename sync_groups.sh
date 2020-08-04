ae0f3e26e838d5e72898d7fa1be492d5603887ee
Jeremy Lewi <jlewi@google.com>
2020-08-04 10:13:27 -0700

    Create the calendar admins group.
    * Add some Googlers to calendar admins.
    
    Related to kubeflow/community#342

--- sync_groups.sh
+++ sync_groups.sh
@@ -3,7 +3,7 @@
 # Sync groups using the gam CLI
 set -ex
 
-KF_GROUPS=( "ci-team" "ci-viewer" "code-search-team" "devrel-team" "devstats" "drive-content-managers" "example-maintainers" "feast-team" "github-team" "google-kubeflow-admins" "google-team" "insight-fellowship" "kf-demo-owners" "kf-kcc-admins" "kubeflow-examples-gcr-writers" "modeldb-team" "release-team")
+KF_GROUPS=( "calendar-admins" "ci-team" "ci-viewer" "code-search-team" "devrel-team" "devstats" "drive-content-managers" "example-maintainers" "feast-team" "github-team" "google-kubeflow-admins" "google-team" "insight-fellowship" "kf-demo-owners" "kf-kcc-admins" "kubeflow-examples-gcr-writers" "modeldb-team" "release-team")
 for g in "${KF_GROUPS[@]}"
 do
 	gam update group ${g}@kubeflow.org sync member file ${g}.members.txt
