ed9142b6106badc36b37461960587d3233149817
Mathew Wicks <thesuperzapper@users.noreply.github.com>
2020-08-26 13:50:44 +1000

    add new community meeting hosts

--- sync_groups.sh
+++ sync_groups.sh
@@ -3,8 +3,27 @@
 # Sync groups using the gam CLI
 set -ex
 
-KF_GROUPS=( "calendar-admins" "ci-team" "ci-viewer" "code-search-team" "devrel-team" "devstats" "drive-content-managers" "example-maintainers" "feast-team" "github-team" "google-kubeflow-admins" "google-team" "insight-fellowship" "kf-demo-owners" "kf-kcc-admins" "kubeflow-examples-gcr-writers" "modeldb-team" "release-team")
-for g in "${KF_GROUPS[@]}"
-do
-	gam update group ${g}@kubeflow.org sync member file ${g}.members.txt
+KF_GROUPS=(
+  "calendar-admins"
+  "ci-team"
+  "ci-viewer"
+  "code-search-team"
+  "community-meeting-hosts"
+  "devrel-team"
+  "devstats"
+  "drive-content-managers"
+  "example-maintainers"
+  "feast-team"
+  "github-team"
+  "google-kubeflow-admins"
+  "google-team"
+  "insight-fellowship"
+  "kf-demo-owners"
+  "kf-kcc-admins"
+  "kubeflow-examples-gcr-writers"
+  "modeldb-team"
+  "release-team"
+)
+for g in "${KF_GROUPS[@]}"; do
+  gam update group ${g}@kubeflow.org sync member file ${g}.members.txt
 done
